ad55bfac0526d09f5d3b53e670500170e49c538f
wolaafolabi <darasimiisaac13@gmail.com>
2025-06-24 18:14:13 +0100

    first commit

--- home.py
+++ home.py
@@ -19,7 +19,7 @@ def render_home():
         resp = requests.get(banner_url)
         resp.raise_for_status()
         img = Image.open(BytesIO(resp.content))
-        st.image(img, width=700, caption="Banking in its best form")
+        st.image(img, width=700, caption="BANKING IN IT'S FORM")
     except Exception as e:
         st.warning(f"sorry,Could NOT load banner: {e}")
 
@@ -27,15 +27,15 @@ def render_home():
     #the Three feature cards in columns
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.image("https://img.icons8.com/fluency/96/000000/money-bag-euro.png")
+        st.image("https://scontent.fabv2-2.fna.fbcdn.net/o1/v/t0/f2/m340/AQPXvdPKQ44Y_6v19XuwzY2RykKVPc84OwH-moW-LWdrsKu0eptz_xQArrY2FO35zeDR_lkUaZCciNtrMbiTsFCfCCrsAqxBevSfyumGStTAUqbZeD3oHPJME3lbDP-RIFz5azL3VIMsyq2shWv3x0VvlL2zEw.jpeg?stp=s1440x1440&_nc_ht=scontent.fabv2-2.fna.fbcdn.net&_nc_gid=K2ztB4mGRg6IZ0ZaorllXQ&_nc_cat=100&_nc_oc=AdkNmibRjjavg0QHnXQZpYCZScEWJeGo05pVBaKgHN5Yq-RiDAwQ_r8NsqqkRI9xeE4&ccb=9-4&oh=00_AfObP27HgG-c6uwF-wz3yaxUfJemY2VVwduF5MbHlsYE_g&oe=68548DC3&_nc_sid=5b3566")
         st.subheader("Secure Savings")
         st.write("Kindly Deposit & watch your savings grow.")
     with col2:
-        st.image("https://img.icons8.com/color/96/000000/withdraw.png")
+        st.image("https://scontent.fabv2-2.fna.fbcdn.net/o1/v/t0/f2/m340/AQPhYLUyXgOZgyWPMU_RcB-eBk8rq7F-lVsSvPIxDOcD6nvTa3vSxk07hSrGxU-KJOTwmrp2quczEjtjgSarRZ5WB5mxnnAxMQOy8R3TRWHrDlbj-gVKGxfjw5NxIvbY1HZ3YA2EyT5_vHnWoaagrR0GTYc8KA.jpeg?stp=s1440x1440&_nc_ht=scontent.fabv2-2.fna.fbcdn.net&_nc_gid=zyWXvFJXYBi0RqCR5ZB84w&_nc_cat=104&_nc_oc=AdlxUVxO2Q67Ld8n0OMMsDR9fBZkBGJH06mwYOCCFuDz_b-PIAXKkBvTUkP7ufvYX9k&ccb=9-4&oh=00_AfONOPp8hitfCfGdju89Xt_-oGy3R6NwUTy8ks2gth8aRQ&oe=68548107&_nc_sid=5b3566")
         st.subheader("Easy Withdrawals")
         st.write("Fast access to your funds.")
     with col3:
-        st.image("https://img.icons8.com/color/96/000000/real-time-claims.png")
+        st.image("https://scontent.fabv2-2.fna.fbcdn.net/o1/v/t0/f2/m340/AQPjnGt-xGW-4q71XAtAFfR3VLRGZVkmEWPs01neHsSWdt1AMsCH9QvqQ5hHWPDVpZxImUErmF65SVh-zeoxGySJYcwgBzRX9RDg4IJgOH3qsh8oEcomZKC7FdBOZWgR2Y_43aNalEuqTUzKGW834oCh33R46g.jpeg?stp=s1440x1440&_nc_ht=scontent.fabv2-2.fna.fbcdn.net&_nc_gid=qEBzUGT9tVOAPV6VIgAzcQ&_nc_cat=111&_nc_oc=AdnGPJr-FaTDjNSgu6rEe8ruq33TgBTpq_8HdyI0KIfxtvHa6c9r1CYOPddCXevVOd4&ccb=9-4&oh=00_AfNQkq_hoYFBuL4UULNrMHpAflN3TwX0YRopwkUDchk3Kg&oe=68547FDA&_nc_sid=5b3566")
         st.subheader("Instant Alerts")
         st.write("Get notifications for every transaction you make.")
     st.markdown("---")
